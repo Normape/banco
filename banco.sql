0e67684b8a405c06801d0b6fde234557b5e0f1af
wapad15 <50056718+wapad15@users.noreply.github.com>
2019-07-02 20:37:26 -0500

    actualización de  la base  de datos con   las nuevas tablas
    se agregaron nuevas tablas (saldo  y mvto)

--- banco.sql
+++ banco.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generaciÃ³n: 03-07-2019 a las 03:32:47
+-- VersiÃ³n del servidor: 10.3.15-MariaDB
+-- VersiÃ³n de PHP: 7.3.6
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -27,21 +36,51 @@ CREATE TABLE `cliente` (
   `email` varchar(45) COLLATE utf8_bin NOT NULL,
   `telefono` varchar(45) COLLATE utf8_bin NOT NULL,
   `celular` varchar(45) COLLATE utf8_bin NOT NULL,
-  `saldo` int(11) NOT NULL
-);
+  `saldo` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
 INSERT INTO `cliente` (`id`, `cedula`, `nombre`, `direccion`, `email`, `telefono`, `celular`, `saldo`) VALUES
-(1, '123456', 'wilder', 'mandela', 'wilder@cc.com', '123456789', '987654321', 8000),
-(4, '1234567', 'osnaider miranda', 'mandela', 'osnaider@cc.com', '456789', '654987', 22000),
-(5, '321654987', 'norma', 'ulala', 'norma@cc.com', '6542156', '565635296', 20000),
-(10, '6548525', 'kevin', 'qwqwf', 'kevin@cc.com', '2545', '21545', 54646);
+(1, '123456', 'wilder', 'mandela', 'wilder@cc.com', '123456789', '987654321', 15000),
+(4, '1234567', 'osnaider miranda', 'mandela', 'osnaider@cc.com', '456789', '654987', 15000),
+(5, '321654987', 'norma', 'ulala', 'norma@cc.com', '6542156', '565635296', 35000),
+(10, '6548525', 'kevin', 'qwqwf', 'kevin@cc.com', '2545', '21545', 54646),
+(11, '123456789', 'norma patricia', 'colombia', 'normaP@gmail.com', '987654', '456789', 15000);
+
+-- --------------------------------------------------------
 
 --
--- Índices para tablas volcadas
+-- Estructura de tabla para la tabla `mvto`
+--
+
+CREATE TABLE `mvto` (
+  `id_cuenta` int(11) NOT NULL,
+  `fec_movimiento` date NOT NULL,
+  `hora` varchar(10) COLLATE utf8_bin NOT NULL,
+  `tipo` varchar(2) COLLATE utf8_bin NOT NULL,
+  `valor` int(11) NOT NULL,
+  `secuencia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `saldo`
+--
+
+CREATE TABLE `saldo` (
+  `NRO_CUENTA` int(10) NOT NULL,
+  `ingreso` int(11) NOT NULL,
+  `egreso` int(11) NOT NULL,
+  `saldo` int(11) NOT NULL,
+  `cedula` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Ãndices para tablas volcadas
 --
 
 --
@@ -52,6 +91,19 @@ ALTER TABLE `cliente`
   ADD UNIQUE KEY `cedula` (`cedula`);
 
 --
+-- Indices de la tabla `mvto`
+--
+ALTER TABLE `mvto`
+  ADD PRIMARY KEY (`id_cuenta`);
+
+--
+-- Indices de la tabla `saldo`
+--
+ALTER TABLE `saldo`
+  ADD PRIMARY KEY (`NRO_CUENTA`),
+  ADD UNIQUE KEY `cedula` (`cedula`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -59,6 +111,9 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
